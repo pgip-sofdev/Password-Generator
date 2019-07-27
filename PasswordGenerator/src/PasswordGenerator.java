362e89e4d1ca19b2d4b8b84836066d78ebcca3c3
blood <blood@192.168.1.10>
2019-07-27 13:43:34 -0400

    First code

---
+++ PasswordGenerator/src/PasswordGenerator.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int length = 12;
+		System.out.println(password(length));
+		
+	}
+		static char[] password(int len)
+		{
+			System.out.println("Create password using random ()");
+			System.out.println("The new password is:  ");
+			
+		String Captial_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String Lower_chars = "abcdefghijklmnopqrstuvwxyz";
+		String Numbers = "0123456789";
+		String Symbols = "!@#$%^&*?<>";
+		
+		String values = Captial_chars + Lower_chars + Numbers + Symbols ;
+		
+		Random rndm_method = new Random ();
+		
+		char[] password = new char [len];
+		
+		for (int i = 0; i < len; i ++)
+		{
+			
+			password[i] = values.charAt(rndm_method.nextInt(values.length()));
+		
+		}
+		
+		return password;
+		}
+}
